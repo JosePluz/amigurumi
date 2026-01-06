99be54d5d6c42ebe64573a29631fbcd4df7bc2b3
JosePluz <josepluz00@gmail.com>
2026-01-06 15:24:12 -0600

    📦 Admin Update: 1 productos

--- products.js
+++ products.js
@@ -1,21 +1,21 @@
 /**
  * Catálogo de Productos - Amigurumis
- * Generado: 1/6/2026, 9:14:51 PM
+ * Generado: 1/6/2026, 9:24:11 PM
  * Total productos: 1
  */
 
 export const products = [
   {
-    "id": 1767734083480,
-    "name": "ggeh",
-    "desc": "fd",
-    "price": 43443,
+    "id": 1767734636153,
+    "name": "vvggv",
+    "desc": "vghvghvhgvhv",
+    "price": 766,
     "size": {
-      "width": 34,
-      "height": 43
+      "width": 67,
+      "height": 45
     },
     "imgs": [
-      "img/unnamed.jpg"
+      "img/Captura-de-pantalla-2026-01-05-142822.png"
     ]
   }
 ];
